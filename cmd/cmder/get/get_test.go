8fa4429983f6b8e1fa439f35c65a594904756251
photowey <photowey@gmail.com>
2026-03-27 00:45:39 +0800

    refactor: update license header(codequality/HEADER)

--- cmd/cmder/get/get_test.go
+++ cmd/cmder/get/get_test.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2023-present the keepass authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package get
 
 import (
@@ -8,11 +24,12 @@ import (
 	"testing"
 	"time"
 
+	_ "unsafe"
+
 	"github.com/photowey/keepass/configs"
 	"github.com/photowey/keepass/internal/manager"
 	"github.com/photowey/keepass/internal/testutil"
 	"github.com/photowey/keepass/internal/vault"
-	_ "unsafe"
 )
 
 //go:linkname clipboardWriteAll github.com/photowey/keepass/internal/clipboard.writeAll
--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/photowey/keepass
 
-go 1.24
+go 1.25.0
 
 require (
 	github.com/atotto/clipboard v0.1.4
-	github.com/spf13/cobra v1.8.0
-	golang.org/x/crypto v0.15.0
-	golang.org/x/term v0.14.0
+	github.com/spf13/cobra v1.10.2
+	golang.org/x/crypto v0.49.0
+	golang.org/x/term v0.41.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	golang.org/x/sys v0.42.0 // indirect
 )
